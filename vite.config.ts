c33fe3b4ebc744aaf1e1aa28c4eee3a699f73718
mostafa1672013 <mostafaomar1672013@gmail.com>
2026-05-04 17:34:01 +0300

    Configure base path and aliases in vite.config.ts
    Set base path for GitHub Pages and configure aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,21 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 1. تحديد المسار الأساسي ليفتح الموقع على جيت هب بدون صفحة بيضاء
+  base: '/School-Education/', 
+  
   server: {
     host: "::",
     port: 8080,
   },
+  
   plugins: [
     react(),
   ],
+  
   resolve: {
     alias: {
+      // 2. إعداد الاختصارات لتسهيل استدعاء الملفات داخل مجلد src
       "@": path.resolve(__dirname, "./src"),
     },
   },
